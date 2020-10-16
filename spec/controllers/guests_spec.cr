3de7138e803cb432f3277e2c2e945aacf53dbd5d
dukeraphaelng <duke.raphael.ng@gmail.com>
2020-10-16 12:37:38 +1100

    chore: refactor response.to_s.split to crystal context.response.output

--- spec/controllers/guests_spec.cr
+++ spec/controllers/guests_spec.cr
@@ -23,24 +23,24 @@ describe Guests do
 
   it "should return a list of guests" do
     # instantiate the controller
-    response = IO::Memory.new
-    app = Guests.new(context("GET", "/api/staff/v1/guests", HEADERS, response_io: response))
+    ctx = context("GET", "/api/staff/v1/guests", HEADERS)
+    ctx.response.output = IO::Memory.new
+    Guests.new(ctx).index
 
     # Test the instance method of the controller
-    app.index
-    response.to_s.split("\r\n").reject(&.empty?)[-1].should eq(
+    ctx.response.output.to_s.should eq(
       %([{"email":"jon@place.techn","name":"Jon","preferred_name":null,"phone":null,"organisation":null,"notes":null,"photo":null,"banned":false,"dangerous":false,"extension_data":{},"checked_in":false,"visit_expected":false},{"email":"steve@place.techn","name":"Steve","preferred_name":null,"phone":null,"organisation":null,"notes":null,"photo":null,"banned":false,"dangerous":false,"extension_data":{},"checked_in":false,"visit_expected":false}])
     )
   end
 
   it "should return a filtered list of guests" do
     # instantiate the controller
-    response = IO::Memory.new
-    app = Guests.new(context("GET", "/api/staff/v1/guests?q=stev", HEADERS, response_io: response))
+    ctx = context("GET", "/api/staff/v1/guests?q=stev", HEADERS)
+    ctx.response.output = IO::Memory.new
+    Guests.new(ctx).index
 
     # Test the instance method of the controller
-    app.index
-    response.to_s.split("\r\n").reject(&.empty?)[-1].should eq(
+    ctx.response.output.to_s.should eq(
       %([{"email":"steve@place.techn","name":"Steve","preferred_name":null,"phone":null,"organisation":null,"notes":null,"photo":null,"banned":false,"dangerous":false,"extension_data":{},"checked_in":false,"visit_expected":false}])
     )
   end
@@ -49,25 +49,26 @@ describe Guests do
     now = Time.utc.to_unix
     later = 4.hours.from_now.to_unix
     route = "/api/staff/v1/guests?period_start=#{now}&period_end=#{later}"
-    response = IO::Memory.new
-    app = Guests.new(context("GET", route, HEADERS, response_io: response))
+    ctx = context("GET", route, HEADERS)
+    ctx.response.output = IO::Memory.new
+    Guests.new(ctx).index
 
     # Test the instance method of the controller
-    app.index
-    response.to_s.split("\r\n").reject(&.empty?)[-1].should eq("[]")
+    ctx.response.output.to_s.should eq("[]")
 
     meta = generate_event
     guest = generate_guest
     guest.attendee_for(meta.id.not_nil!)
 
     # instantiate the controller
-    response = IO::Memory.new
-    app = Guests.new(context("GET", route, HEADERS, response_io: response))
+    ctx = context("GET", route, HEADERS)
+    ctx.response.output = IO::Memory.new
+    app = Guests.new(ctx)
 
     begin
       # Test the instance method of the controller
       app.index
-      response.to_s.split("\r\n").reject(&.empty?)[-1].should eq(
+      ctx.response.output.to_s.should eq(
         "[{\"email\":\"bob@outside.com\",\"name\":null,\"preferred_name\":null,\"phone\":null,\"organisation\":null,\"notes\":null,\"photo\":null,\"banned\":false,\"dangerous\":false,\"extension_data\":{},\"checked_in\":false,\"visit_expected\":true}]"
       )
     ensure
@@ -94,21 +95,23 @@ describe Guests do
       now = Time.utc.to_unix
       later = 4.hours.from_now.to_unix
       route = "/api/staff/v1/guests?period_start=#{now}&period_end=#{later}&system_ids=sys-rJQQlR4Cn7"
-      response = IO::Memory.new
-      app = Guests.new(context("GET", route, HEADERS, response_io: response))
+
+      ctx = context("GET", route, HEADERS)
+      ctx.response.output = IO::Memory.new
+      Guests.new(ctx).index
 
       # Test the instance method of the controller
-      app.index
-      response.to_s.split("\r\n").reject(&.empty?)[-1].should eq("[]")
+      ctx.response.output.to_s.should eq("[]")
 
       # instantiate the controller
-      response = IO::Memory.new
+
       route = "/api/staff/v1/guests?period_start=#{now}&period_end=#{later}&system_ids=sys-rJQQlR4Cn7,sys_id"
-      app = Guests.new(context("GET", route, HEADERS, response_io: response))
+      ctx = context("GET", route, HEADERS)
+      ctx.response.output = IO::Memory.new
+      Guests.new(ctx).index
 
       # Test the instance method of the controller
-      app.index
-      response.to_s.split("\r\n").reject(&.empty?)[-1].should eq(
+      ctx.response.output.to_s.should eq(
         "[{\"email\":\"bob@outside.com\",\"name\":null,\"preferred_name\":null,\"phone\":null,\"organisation\":null,\"notes\":null,\"photo\":null,\"banned\":false,\"dangerous\":false,\"extension_data\":{},\"checked_in\":false,\"visit_expected\":true}]"
       )
     ensure
@@ -119,51 +122,49 @@ describe Guests do
 
   it "should show a guests details" do
     # instantiate the controller
-    response = IO::Memory.new
-    context = context("GET", "/api/staff/v1/guests/#{guest1.email}/", HEADERS, response_io: response)
-    context.route_params = {"id" => guest1.email.not_nil!}
-    app = Guests.new(context)
+    ctx = context("GET", "/api/staff/v1/guests/#{guest1.email}/", HEADERS)
+    ctx.route_params = {"id" => guest1.email.not_nil!}
+    ctx.response.output = IO::Memory.new
+    Guests.new(ctx).show
 
     # Test the instance method of the controller
-    app.show
-    response.to_s.split("\r\n").reject(&.empty?)[-1].should eq(
+    ctx.response.output.to_s.should eq(
       %({"email":"steve@place.techn","name":"Steve","preferred_name":null,"phone":null,"organisation":null,"notes":null,"photo":null,"banned":false,"dangerous":false,"extension_data":{},"checked_in":false,"visit_expected":false})
     )
   end
 
   it "should show a guests details when visiting today" do
     # instantiate the controller
-    response = IO::Memory.new
-    context = context("GET", "/api/staff/v1/guests/#{guest1.email}/", HEADERS, response_io: response)
-    context.route_params = {"id" => guest1.email.not_nil!}
-    app = Guests.new(context)
+    ctx = context("GET", "/api/staff/v1/guests/#{guest1.email}/", HEADERS)
+    ctx.route_params = {"id" => guest1.email.not_nil!}
+    ctx.response.output = IO::Memory.new
+    app = Guests.new(ctx)
 
     meta = generate_event
     guest1.attendee_for(meta.id.not_nil!)
 
     # Test the instance method of the controller
     app.show
-    response.to_s.split("\r\n").reject(&.empty?)[-1].should eq(
+    ctx.response.output.to_s.should eq(
       %({"email":"steve@place.techn","name":"Steve","preferred_name":null,"phone":null,"organisation":null,"notes":null,"photo":null,"banned":false,"dangerous":false,"extension_data":{},"checked_in":false,"visit_expected":true})
     )
   end
 
   it "should delete a guest" do
     # instantiate the controller
-    context = context("DELETE", "/api/staff/v1/guests/#{guest1.email}/", HEADERS)
-    context.route_params = {"id" => guest1.email.not_nil!}
-    app = Guests.new(context)
+    ctx = context("DELETE", "/api/staff/v1/guests/#{guest1.email}/", HEADERS)
+    ctx.route_params = {"id" => guest1.email.not_nil!}
 
     # Test the instance method of the controller
-    app.destroy
+    Guests.new(ctx).destroy
 
     # Check only one is returned
-    response = IO::Memory.new
-    app = Guests.new(context("GET", "/api/staff/v1/guests", HEADERS, response_io: response))
+    ctx = context("GET", "/api/staff/v1/guests", HEADERS)
+    ctx.response.output = IO::Memory.new
 
     # Test the instance method of the controller
-    app.index
-    response.to_s.split("\r\n").reject(&.empty?)[-1].should eq(
+    Guests.new(ctx).index
+    ctx.response.output.to_s.should eq(
       %([{"email":"jon@place.techn","name":"Jon","preferred_name":null,"phone":null,"organisation":null,"notes":null,"photo":null,"banned":false,"dangerous":false,"extension_data":{},"checked_in":false,"visit_expected":false}])
     )
   end
@@ -173,12 +174,11 @@ describe Guests do
     body = IO::Memory.new
     body << %({"email":"bob@jane.com","banned":true,"extension_data":{"test":"data"}})
     body.rewind
-    response = IO::Memory.new
-    context = context("POST", "/api/staff/v1/guests/", HEADERS, body, response_io: response)
-    app = Guests.new(context)
-    app.create
+    ctx = context("POST", "/api/staff/v1/guests/", HEADERS, body)
+    ctx.response.output = IO::Memory.new
+    Guests.new(ctx).create
 
-    response.to_s.split("\r\n").reject(&.empty?)[-1].should eq(
+    ctx.response.output.to_s.should eq(
       %({"email":"bob@jane.com","name":null,"preferred_name":null,"phone":null,"organisation":null,"notes":null,"photo":null,"banned":true,"dangerous":false,"extension_data":{"test":"data"},"checked_in":false,"visit_expected":false})
     )
 
@@ -186,13 +186,12 @@ describe Guests do
     body = IO::Memory.new
     body << %({"name":"Bob Jane","extension_data":{"other":"stuff"}})
     body.rewind
-    response = IO::Memory.new
-    context = context("PATCH", "/api/staff/v1/guests/bob@jane.com", HEADERS, body, response_io: response)
-    context.route_params = {"id" => "bob@jane.com"}
-    app = Guests.new(context)
-    app.update
+    ctx = context("PATCH", "/api/staff/v1/guests/bob@jane.com", HEADERS, body)
+    ctx.route_params = {"id" => "bob@jane.com"}
+    ctx.response.output = IO::Memory.new
+    Guests.new(ctx).update
 
-    response.to_s.split("\r\n").reject(&.empty?)[-1].should eq(
+    ctx.response.output.to_s.should eq(
       %({"email":"bob@jane.com","name":"Bob Jane","preferred_name":null,"phone":null,"organisation":null,"notes":null,"photo":null,"banned":false,"dangerous":false,"extension_data":{"test":"data","other":"stuff"},"checked_in":false,"visit_expected":false})
     )
   end
