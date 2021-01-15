06b33bf6581c2fd59aad3729b0f394cc511329d3
Stephen von Takach <steve@vontaka.ch>
2021-01-15 15:21:47 +1100

    feat(guests): raise error on invalid param

--- src/controllers/guests.cr
+++ src/controllers/guests.cr
@@ -198,6 +198,6 @@ class Guests < Application
 
   def find_guest
     # Find will raise a 404 (not found) if there is an error
-    Guest.find!(route_params["id"])
+    Guest.find!(route_params["id"].to_i64)
   end
 end
