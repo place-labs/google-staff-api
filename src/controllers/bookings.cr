5d4d32cbd03c36acec143c24cef8f36dea299d04
Stephen von Takach <steve@vontaka.ch>
2021-01-27 20:05:43 +1100

    chore(shard.lock): bump versions
    google calendar listing should run through the pages

--- src/controllers/bookings.cr
+++ src/controllers/bookings.cr
@@ -69,19 +69,23 @@ class Bookings < Application
 
     if booking.save
       spawn do
-        get_placeos_client.root.signal("staff/booking/changed", {
-          action:        :create,
-          id:            booking.id,
-          booking_type:  booking.booking_type,
-          booking_start: booking.booking_start,
-          booking_end:   booking.booking_end,
-          timezone:      booking.timezone,
-          resource_id:   booking.asset_id,
-          user_id:       booking.user_id,
-          user_email:    booking.user_email,
-          user_name:     booking.user_name,
-          zones:         booking.zones,
-        })
+        begin
+          get_placeos_client.root.signal("staff/booking/changed", {
+            action:        :create,
+            id:            booking.id,
+            booking_type:  booking.booking_type,
+            booking_start: booking.booking_start,
+            booking_end:   booking.booking_end,
+            timezone:      booking.timezone,
+            resource_id:   booking.asset_id,
+            user_id:       booking.user_id,
+            user_email:    booking.user_email,
+            user_name:     booking.user_name,
+            zones:         booking.zones,
+          })
+        rescue error
+          Log.error(exception: error) { "while signaling booking created" }
+        end
       end
 
       render json: booking, status: HTTP::Status::CREATED
@@ -156,19 +160,23 @@ class Bookings < Application
     booking.destroy
 
     spawn do
-      get_placeos_client.root.signal("staff/booking/changed", {
-        action:        :cancelled,
-        id:            booking.id,
-        booking_type:  booking.booking_type,
-        booking_start: booking.booking_start,
-        booking_end:   booking.booking_end,
-        timezone:      booking.timezone,
-        resource_id:   booking.asset_id,
-        user_id:       booking.user_id,
-        user_email:    booking.user_email,
-        user_name:     booking.user_name,
-        zones:         booking.zones,
-      })
+      begin
+        get_placeos_client.root.signal("staff/booking/changed", {
+          action:        :cancelled,
+          id:            booking.id,
+          booking_type:  booking.booking_type,
+          booking_start: booking.booking_start,
+          booking_end:   booking.booking_end,
+          timezone:      booking.timezone,
+          resource_id:   booking.asset_id,
+          user_id:       booking.user_id,
+          user_email:    booking.user_email,
+          user_name:     booking.user_name,
+          zones:         booking.zones,
+        })
+      rescue error
+        Log.error(exception: error) { "while signaling booking cancelled" }
+      end
     end
 
     head :accepted
@@ -193,19 +201,23 @@ class Bookings < Application
   def update_booking(booking, signal = "changed")
     if booking.save
       spawn do
-        get_placeos_client.root.signal("staff/booking/changed", {
-          action:        signal,
-          id:            booking.id,
-          booking_type:  booking.booking_type,
-          booking_start: booking.booking_start,
-          booking_end:   booking.booking_end,
-          timezone:      booking.timezone,
-          resource_id:   booking.asset_id,
-          user_id:       booking.user_id,
-          user_email:    booking.user_email,
-          user_name:     booking.user_name,
-          zones:         booking.zones,
-        })
+        begin
+          get_placeos_client.root.signal("staff/booking/changed", {
+            action:        signal,
+            id:            booking.id,
+            booking_type:  booking.booking_type,
+            booking_start: booking.booking_start,
+            booking_end:   booking.booking_end,
+            timezone:      booking.timezone,
+            resource_id:   booking.asset_id,
+            user_id:       booking.user_id,
+            user_email:    booking.user_email,
+            user_name:     booking.user_name,
+            zones:         booking.zones,
+          })
+        rescue error
+          Log.error(exception: error) { "while signaling booking #{signal}" }
+        end
       end
 
       render json: booking
